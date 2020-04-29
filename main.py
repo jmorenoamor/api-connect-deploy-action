94f2e6fe919bc1c73a15dae4813d8f40893ab3d1
Jesus Moreno Amor <jesus@morenoamor.com>
2020-04-29 17:40:29 +0200

    Logging

--- main.py
+++ main.py
@@ -25,33 +25,55 @@ __email__ = "jesus@morenoamor.com"
 __status__ = "Production"
 
 
-class APIDeployError(Exception):
-    def __init__(self, message, errors):
-        super().__init__(message)
-        self.errors = errors
+logging.config.dictConfig({ 
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': { 
+        'standard': { 'format': '[%(levelname)s] - %(message)s' },
+        'github':   { 'format': '::%(levelname)s::%(message)s' },
+    },
+    'handlers': { 
+        'default': { 'level': 'DEBUG', 'formatter': 'standard', 'class': 'logging.StreamHandler', 'stream': 'ext://sys.stdout' },
+        'github':  { 'level': 'DEBUG', 'formatter': 'github', 'class': 'logging.StreamHandler', 'stream': 'ext://sys.stdout' },
+    },
+    'loggers': { 
+        '':       { 'handlers': ['default'], 'level': 'DEBUG', 'propagate': False },
+        'github': {  'handlers': ['github'], 'level': 'DEBUG', 'propagate': False }
+    } 
+})
+
+logging.addLevelName(logging.DEBUG, 'debug')
+logging.addLevelName(logging.INFO, 'debug')
+logging.addLevelName(logging.WARNING, 'warning')
+logging.addLevelName(logging.ERROR, 'error')
+
+logger = logging.getLogger()
+github_logger = logging.getLogger('github')
+
+# class APIDeployError(Exception):
+#     def __init__(self, message, errors):
+#         super().__init__(message)
+#         self.errors = errors
 
 
 def load_yaml(filename, encoding='utf-8'):
     with open(filename, 'r', encoding=encoding) as file:
-        try:
-            return yaml.safe_load(file)
-        except yaml.YAMLError as ex:
-            raise APIDeployError("Error al cargar el archivo: " + filename, None)
+        return yaml.safe_load(file)
 
 
-def setup_logging(path='/app/logging.yaml', default_level=logging.INFO):
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            try:
-                config = yaml.safe_load(f.read())
-                logging.config.dictConfig(config)
-            except Exception as e:
-                print(e)
-                print('Error in Logging Configuration. Using default configs')
-                logging.basicConfig(level=default_level)
-    else:
-        logging.basicConfig(level=default_level)
-        print('Failed to load configuration file. Using default configs')
+# def setup_logging(path='/app/logging.yaml', default_level=logging.INFO):
+#     if os.path.exists(path):
+#         with open(path, 'rt') as f:
+#             try:
+#                 config = yaml.safe_load(f.read())
+#                 logging.config.dictConfig(config)
+#             except Exception as e:
+#                 print(e)
+#                 print('Error in Logging Configuration. Using default configs')
+#                 logging.basicConfig(level=default_level)
+#     else:
+#         logging.basicConfig(level=default_level)
+#         print('Failed to load configuration file. Using default configs')
 
 
 def prepare_product(product_file):
@@ -70,9 +92,9 @@ def prepare_product(product_file):
             If the API is defined by name and version, we need to search through the filesystem
             for a yaml fil that contains the API and version specified
             '''
-            raise APIDeployError("Formato de producto no soportado 'name:'", None)
+            raise Exception("Formato de producto no soportado 'name:'", None)
         if "$ref" in api_definition.keys():
-            console_logger.debug(f"API $ref {api_definition['$ref']}")
+            logger.debug(f"API $ref {api_definition['$ref']}")
             '''
             If the API is defined as a external reference, we need to solve it and transform the
             definition from:
@@ -84,7 +106,7 @@ def prepare_product(product_file):
 
             # Clean the API reference name, solo para VODAFONE
             clean_name = api_definition['$ref'].split('_')[0] + ".yaml"
-            console_logger.debug(f"Cleaned {api_definition['$ref']} to {clean_name}")
+            logger.debug(f"Cleaned {api_definition['$ref']} to {clean_name}")
 
             # Load the API
             api_filename = os.path.join(product_path, clean_name)
@@ -92,14 +114,14 @@ def prepare_product(product_file):
 
             # Transform the reference from $ref to name
             api_definition['name'] = f"{api['info']['x-ibm-name']}:{api['info']['version']}"
-            console_logger.debug(f"Translated {api_definition['$ref']} to {api_definition['name']}")
+            logger.debug(f"Translated {api_definition['$ref']} to {api_definition['name']}")
             del api_definition['$ref']
 
             # Add the API file to the publish order
             files.append(
                 ('openapi', ('openapi', open(api_filename, 'rb'), 'application/yaml'))
             )
-            console_logger.info(f"Added API {api_filename} to the publish order")
+            logger.info(f"Added API {api_filename} to the publish order")
 
             # If the API has a WSDL definition, add it to the publish order
             if api['x-ibm-configuration']['type'] == "wsdl" and 'wsdl-definition' in api['x-ibm-configuration']:
@@ -107,7 +129,7 @@ def prepare_product(product_file):
                 files.append(
                     ('wsdl', ('wsdl', open(wsdl_filename, 'rb'), 'application/zip'))
                 )
-                console_logger.info(f"Added WSDL {wsdl_filename} to the publish order")
+                logger.info(f"Added WSDL {wsdl_filename} to the publish order")
 
     # Dump the product to a temporal file that doesn't have $ref references
     temporal_product = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'to_deploy.yaml')
@@ -118,7 +140,7 @@ def prepare_product(product_file):
     files.append(
         ('product', ('product', open(temporal_product, 'rb'), 'application/yaml')),
     )
-    console_logger.info(f"Added product {temporal_product} to the publish order")
+    logger.info(f"Added product {temporal_product} to the publish order")
 
     # Publish the product
     # published_product = apic.product_publish(organization, catalog, product, files, space)
@@ -143,7 +165,7 @@ def main():
     # Login
     apic.login(manager_usrname, manager_password, manager_realm)
     # print("::debug::Logged in to API Connect")
-    console_logger.info(f"Logged in to API Conned")
+    github_logger.info(f"Logged in to API Conned")
 
     # Prepare the product
     product = load_yaml(product_file)
@@ -152,27 +174,17 @@ def main():
     # Publish the product
     published_product = apic.product_publish(organization, catalog, None, prepared_files, space)
     # print("::debug::Published the product")
-    console_logger.info("Published the product")
+    github_logger.info("Published the product")
 
     # Get product status
     product_version = product['info']['version']
     product_name = product['info']['name']
     product_b = apic.product_get(organization, catalog, product_name, product_version)
-    console_logger.info("Checked the product")
+    github_logger.info("Checked the product")
 
     print(f"::set-output name=result::published")
 
 if __name__ == "__main__":
-
-    setup_logging()
-    logging.addLevelName(logging.DEBUG, 'debug')
-    logging.addLevelName(logging.INFO, 'debug')
-    logging.addLevelName(logging.WARNING, 'warning')
-    logging.addLevelName(logging.ERROR, 'error')
-
-    github_logger = logging.getLogger('github')
-    console_logger = logging.getLogger('console')
-
     try:
         main()
         exit(0)
